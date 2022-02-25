8f3199784dcfb3509bf72b1bf4f57cf4377c4dad
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-25 09:24:26 +0200

    Initial Commit

---
+++ MobiLabShoppingApp/MobiLabShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiLabShoppingAppApp.swift
+//  MobiLabShoppingApp
+//
+//  Created by ramil on 25.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobiLabShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
